1e6061edc2b3ec670787662dfcf8476a37fe3ad1
Eng Md Ektiar Hossen <97474187+engektiar@users.noreply.github.com>
2022-05-12 12:59:58 +0600

    auido and text add

---
+++ js/index.js
@@ -0,0 +1,39 @@
+var len = document.querySelectorAll(".mybtn").length;
+for(var i=0; i<len; i++){
+  document.querySelectorAll(".mybtn")[i].addEventListener("click",function(){
+    var text=this.innerHTML;
+    document.querySelector("h1").innerHTML=text+ "is cleeked";
+  })
+} 
+
+// Audio add 
+var audiolen=document.querySelectorAll(".mybutton").length;
+for(var i=0; i<audiolen; i++){
+  document.querySelectorAll(".mybutton")[i].addEventListener("click",function(){
+    var mytext=this.innerHTML;
+  console.log(mytext);
+
+    switch(mytext){
+       case "a":
+        var audio = new Audio('sound/a (1).mp3');
+        audio.play();
+        break;
+
+        case "b":
+        var audio = new Audio('sound/a (2).mp3');
+        audio.play();
+        break;
+
+        case "c":
+          var audio = new Audio('sound/a (3).mp3');
+          audio.play();
+          break;
+
+          case "d":
+            var audio = new Audio('sound/a (4).mp3');
+            audio.play();
+            break;
+         
+    }
+  })
+}
